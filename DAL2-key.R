d6ae93a37eaab66815f5eae37b105ed97367e755
Grant Allard <grantallard.dev@gmail.com>
2021-01-25 11:20:19 -0500

    Updated DAL2

--- DAL2-key.R
+++ DAL2-key.R
@@ -514,21 +514,14 @@ flights %>%
   geom_bar(stat='identity') +
   xlab('Number of Flights') # Why did I add this label to this version of the ggplot? 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> d85b6c80009ef5a1b7158c36fdd74665d4800e6d
 # Which carrier had the shortest mean arrival delay? 
 flights %>% 
   group_by(carrier) %>% 
   summarize(mean_arrival_delay = mean(arr_delay, na.rm = TRUE)) %>% 
   select(carrier, mean_arrival_delay ) %>% 
   arrange(mean_arrival_delay)
-<<<<<<< HEAD
-# Alaska Airlines, on average, had the best on time performance.   
-=======
-  
->>>>>>> d85b6c80009ef5a1b7158c36fdd74665d4800e6d
+
+# Alaska Airlines, on average, had the best on-time performance.
 
 # Which carrier experienced the most canceled flights?
 flights %>% 
@@ -553,7 +546,5 @@ flights %>%
   group_by(carrier) %>% 
   summarize(mean_speed = mean(speed, na.rm = TRUE) ) %>% 
   arrange(desc(mean_speed))
-
-
 
 # Copyright (c) Grant Allard, 2021
